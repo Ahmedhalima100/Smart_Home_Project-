6c6e2c6dfa5b979c6653a7b4a94379a364a2c703
Ahmed Hassan <ahmedrhassan98@gmail.com>
2021-04-21 16:09:20 +0200

    gg

--- Slave/released/Debug/DIO_program.d
+++
@@ -1,7 +0,0 @@
-DIO_program.o: ../DIO_program.c ../STD_TYPES.h ../BIT_MATH.h \
- ../Compiler.h ../DIO_interface.h ../AVR_DIO_REG.h
-../STD_TYPES.h:
-../BIT_MATH.h:
-../Compiler.h:
-../DIO_interface.h:
-../AVR_DIO_REG.h:
--- Slave/released/Debug/LED.d
+++
@@ -1,5 +0,0 @@
-LED.o: ../LED.c ../BIT_MATH.h ../STD_TYPES.h ../DIO_interface.h ../LED.h
-../BIT_MATH.h:
-../STD_TYPES.h:
-../DIO_interface.h:
-../LED.h:
